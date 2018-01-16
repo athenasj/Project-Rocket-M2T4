075f9a0c6fe2855cdecb60e23f0cf5376a482fee
athenasj <athenasj_23@yahoo.com>
2018-01-16 16:36:45 +0800

    Few edits

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,12 +6,10 @@ using UnityEngine.SceneManagement;
 public class Rocket : MonoBehaviour {
 
     Rigidbody _rigidBody;
-    Vector3 pos;
 
 	// Use this for initialization
 	void Start () {
         _rigidBody = GetComponent<Rigidbody>();
-        pos = transform.position;
 	}
 	
 	// Update is called once per frame
@@ -25,7 +23,7 @@ public class Rocket : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space))
         {
             print("Rocket Boost");
-            _rigidBody.AddRelativeForce(Vector3.up * 100 * Time.deltaTime);
+            _rigidBody.AddRelativeForce(Vector3.up * 1000 * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
